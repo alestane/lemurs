d2f58ac25f80a49dca038bd2e45b2698e0efbe1d
Nevin Flanagan <nevin@playsmith.net>
2023-08-09 23:31:00 -0400

    Moved C++ build.rs script, C++ source file

--- _8080/cpp8080/src/rs8080.cpp
+++ _8080/cpp8080/src/rs8080.cpp
@@ -1,11 +1,21 @@
 #include "rs8080.h"
 
 extern "C" {
-	i8080::state* new_empty_state(size_t memory);
-	i8080::state* new_state_with(size_t memory, const i8080::state::byte* ram);
+	using i8080::state;
+	using byte = i8080::state::byte;
+
+	state* new_empty_state(size_t memory);
+	state* new_state_with(size_t memory, const byte* ram);
+	void discard_state(void*);
+	const byte* state_outputs(const state* self);
+	byte* state_inputs(state* self);
+	const byte* state_ram(const state* self);
+    const byte* state_vram(const state* self);
 }
 
 namespace i8080 {
+ 	void state::deleter::operator()(state* it) { discard_state(it); } 
+
 	state::owner state::create(word size, const byte* memory)
 	{
 		return owner{ memory ? new_state_with(size, memory) : new_empty_state(size ? size : 0x00010000) };
