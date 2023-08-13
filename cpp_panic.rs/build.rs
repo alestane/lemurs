3e2113d99870941dc35c36d4ada13593a243397e
Nevin Flanagan <nevin@playsmith.net>
2023-08-13 17:20:06 -0400

    Updated general-purpose build.rs script

--- cpp_panic.rs/build.rs
+++ cpp_panic.rs/build.rs
@@ -1,6 +1,18 @@
+#![feature(path_file_prefix)]
+
 fn main() {
-	cc::Build::new()
-	    .cpp(true)
-		.file("src/panic.cpp")
-		.compile("panic");
+	std::fs::read_dir("src").map(
+		|dir| {
+			for source in dir.flatten() {
+				let lib = source.path();
+				if  lib.extension() == Some("cpp".as_ref()) {
+					let Some(name) = lib.file_prefix() else { continue };
+					cc::Build::new()
+						.cpp(true)
+						.file(lib.clone())
+						.compile(name.to_string_lossy().as_ref());	
+				}			
+			}
+		}
+	).ok();
 }
