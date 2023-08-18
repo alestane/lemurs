e9eb8ac435b32ffe60a6d0631f9151aeeca97398
Nevin Flanagan <nevin@playsmith.net>
2023-08-17 21:06:17 -0400

    Updated projects to use a unified build script

--- _8080/cpp8080/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-	cc::Build::new()
-	    .cpp(true)
-	    .include("include")
-		.file("src/rs8080.cpp")
-		.compile("rs8080");
-}
---
+++ cppsrc.rs
@@ -0,0 +1,39 @@
+#![feature(path_file_prefix)]
+
+use std::path::PathBuf;
+use cc::Build;
+
+fn compile<F : FnOnce(&mut Build)> (file: PathBuf, process: Option<F>) -> Result<cc::Build, PathBuf> {
+	if file.extension() == Some("cpp".as_ref()) {
+		let Some(name) = file.file_prefix() else { return Err(file) };
+		let mut job = Build::new();
+		job
+			.cpp(true)
+			.file(file.clone())
+	    	.include("include");
+		if let Some(process) = process {
+			process(&mut job);
+		}
+		job.compile(name.to_string_lossy().as_ref());
+		Ok(job)
+	} else {
+		Err(file)
+	}
+
+}
+
+fn main() {
+	let debug = if std::env::var("DEBUG") == Ok(String::from("true")) { Some( |job: &mut Build| {
+		job
+			.flag("-MDd")
+			.define("_ITERATOR_DEBUG_LEVEL", "2");
+	})
+	} else { None };
+	std::fs::read_dir("src").map(
+		|dir| {
+			for source in dir.flatten() {
+				let _ = compile(source.path(), debug);		
+			}
+		}
+	).ok();
+}
