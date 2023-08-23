ce6f5a9ee9a4bae1ccf216bd8244dca1febd5f0d
Nevin Flanagan <nevin@playsmith.net>
2023-08-23 16:47:23 -0400

    Extended types imported for general crate use.

--- _8080/src/lib.rs
+++ _8080/src/lib.rs
@@ -1,19 +1,22 @@
 #![no_std]
 #![feature(split_array)]
 #![feature(exclusive_range_pattern)]
+#![feature(trait_alias)]
+#![feature(iter_intersperse)]
 
 #[cfg(feature="std")]
 mod foundation {
     extern crate std;
-    pub use std::{boxed, vec, array};
+    pub use std::{boxed, vec, array, convert, num, string, result, ops};
 }
 #[cfg(not(feature="std"))]
 mod foundation {
     extern crate alloc;
-    pub use alloc::{boxed, vec};
-    pub use core::array;
+    pub use alloc::{boxed, vec, string};
+    pub use core::{array, convert, num, result, ops};
 }
-use foundation::*;
+use foundation::{*, string::String, result::Result};
+use foundation::vec::Vec;
 pub use boxed::Box;
 
 mod chip;
