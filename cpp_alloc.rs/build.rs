52123bbefc0f14915fdfa50b065abc215261466f
Nevin Flanagan <nevin@playsmith.net>
2023-09-14 21:29:14 -0400

    Removed panic and alloc cpp projects to separate package.

--- cpp_alloc.rs/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-	cc::Build::new()
-	    .cpp(true)
-		.file("src/alloc.cpp")
-		.compile("alloc");
-}
--- cpp_panic.rs/src/panic.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <stdexcept>
-
-extern "C" {
-	[[noreturn]]
-	int bail(const char* err_text) {
-		throw std::runtime_error{err_text};
-	}
-}
