4a13f2c7062b6da028eea1ba53f7ff6d2b7c0078
Nevin Flanagan <nevin@playsmith.net>
2023-10-04 21:55:36 -0400

    Shortened sub-module imports in integration test.

--- _8080/tests/cpudiag.rs
+++ _8080/tests/cpudiag.rs
@@ -5,7 +5,7 @@ use lemurs_8080::Machine;
 mod src {
     pub mod cp_m;
 }
-use src::*;
+use src::cp_m::*;
 
 #[cfg(debug_assertions)]
 #[test]
@@ -13,8 +13,8 @@ fn exercise() {
 
     println!("currently at {}", std::env::current_dir().unwrap().display());
     let body = std::fs::read("tests/cpudiag.bin").expect("Couldn't load test file.");
-    let mut machine = cp_m::CP_M::with_program(&body);
-    let sample = Machine::new(&mut machine);
+    let mut machine = CP_M::with_program(&body);
+    let sample: Machine<CP_M, _> = Machine::new(&mut machine);
     let mut cycles = 0usize;
     for outcome in sample {
         match outcome {
